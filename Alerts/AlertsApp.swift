d8650adfcf465e9d1f883aee08b8929a17e81462
Bernie FitzGerald <bernardf@live.com.au>
2023-06-20 12:52:41 +1000

    Initial Commit

---
+++ Alerts/AlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertsApp.swift
+//  Alerts
+//
+//  Created by Bernard FitzGerald on 20/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
